2f313f58bb7d01f8c74c4082bf83b7c8909e2a34
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-02-08 19:01:44 -1000

    add helper function _requestj

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 console.log("sanity check");
+
+const _request = function(method, url, callback) {
+  const oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", function() {
+    const data = JSON.parse(this.responseText);
+    return callback.bind(this)(data);
+  });
+  oReq.open(method, url);
+  oReq.send();
+};
